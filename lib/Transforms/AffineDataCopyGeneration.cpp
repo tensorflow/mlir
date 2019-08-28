9e868666d047e8d43e5f82f43e4093b838c710fa
Uday Bondhugula <uday@polymagelabs.com>
2019-08-28 11:21:11 +0530

    pipeline-data-transfer: remove dead tag alloc's and improve test coverage for replaceMemRefUsesWith / pipeline-data-transfer
    - address remaining comments from PR #87 for better test coverage for
      pipeline-data-transfer/replaceAllMemRefUsesWith
    - remove dead tag allocs the same way they are removed for the replaced buffers
    
    Signed-off-by: Uday Bondhugula <uday@polymagelabs.com>

--- lib/Transforms/AffineDataCopyGeneration.cpp
+++ lib/Transforms/AffineDataCopyGeneration.cpp
@@ -562,11 +562,12 @@ LogicalResult AffineDataCopyGeneration::generateCopy(
     prev = std::prev(begin);
 
   // *Only* those uses within the range [begin, end) of 'block' are replaced.
-  replaceAllMemRefUsesWith(memref, fastMemRef,
-                           /*extraIndices=*/{}, indexRemap,
-                           /*extraOperands=*/regionSymbols,
-                           /*domInstFilter=*/&*begin,
-                           /*postDomInstFilter=*/&*postDomFilter);
+  if (failed(replaceAllMemRefUsesWith(memref, fastMemRef,
+                                      /*extraIndices=*/{}, indexRemap,
+                                      /*extraOperands=*/regionSymbols,
+                                      /*domInstFilter=*/&*begin,
+                                      /*postDomInstFilter=*/&*postDomFilter)))
+    llvm_unreachable("memref replacement guaranteed to succeed here");
 
   *nBegin = wasAtStartOfBlock ? block->begin() : std::next(prev);
 
